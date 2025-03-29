598deb9c1d0ef540b3c4d9eee5cfc15d5535678a
Arbaaz Mohamadiqbal Jamadar <73300815+arbaaz29@users.noreply.github.com>
2025-03-28 20:47:58 -0400

    Update ecommerce_1.sql

--- ecommerce_1.sql
+++ ecommerce_1.sql
@@ -7,6 +7,12 @@
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
+CREATE USER 'e_comm_app' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ecomdb.* TO 'e_comm_app';
+ALTER USER 'e_comm_app' REQUIRE SSL;
+FLUSH PRIVILEGES;
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
